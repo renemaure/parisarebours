a012198cc7c5f24e271f68372c916015cf3ea36b
renemaure <pascalrollin290@gmail.com>
2023-09-24 15:38:36 +0200

    rajout fichiers

---
+++ pages/entrer_seruriser.php
@@ -0,0 +1,15 @@
+<form name="entrer_serveur" action="index.php" method="post"id="form-bord">
+	<h4>Entrée réservée aux personnes autorisés</h4>
+	<section>	
+		<input id="pass" type="password" name="acces">
+		&nbsp;&nbsp;
+		<input type="submit" value="OK">
+		&nbsp;&nbsp;
+		<input type="reset" value="recommencer">
+		<article>
+			<p>
+				Entrez votre code d'accés, puis validez avec le bouton "<strong>OK</strong>", sinon vous pouvez recommencer avec naturelement le bouton "<strong>recommencer</strong>".
+			</p>
+		</article>
+	</section>	
+</form>
