b7e903d452f87c6559f7742f97da252508f1b029
fateh kabban <fatihkabbani223@gmail.com>
2023-10-07 15:51:48 +0100

    tooltip ajouter

--- maintenance/test_serveur.php
+++ maintenance/test_serveur.php
@@ -2,35 +2,23 @@
 $file_size = 10 * 1024 * 1024; //  10 MB
 $targetDirectory = 'uploads/';
 $fileStatus = '';
-$error_state = '';
 if(!file_exists($targetDirectory)){
     mkdir($targetDirectory , 0777 , true);
 }
 if(isset($_POST['folder_name'])) {
-    $uploadsDir = 'uploads/';
     $newFolderName = $_POST['folder_name'];
-    $newFolderPath =  $uploadsDir .$newFolderName;
-
-    if (!file_exists($newFolderPath)) {
-        mkdir($newFolderPath, 0777, true);
-        echo "New folder created successfully at " . $newFolderPath;
-    } else {
-        echo "Folder already exists at " . $newFolderPath;
-    }
+    $newFolderPath =  $targetDirectory . $newFolderName;
+    mkdir($newFolderPath, 0777, true);
 }
 if(isset($_POST['dossier_choisi'])){
-    $dossier_choisi = $_POST['dossier_choisi'];
-    $targetDirectory = 'uploads/' . $dossier_choisi . '/';
-       if(!file_exists($targetDirectory)){
-            mkdir($targetDirectory , 0777 , true);
-        }
-    echo $targetDirectory;
+    // cree un cookie qui contient le nom du dossier choisi
+    setcookie('dossier_choisi', $_POST['dossier_choisi'], time() + 3600, '/'); // 3600 = 1 heure
 }
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['files'])) {
-    $fileStatus = array(); // une array qui contient les messages d'erreur
-    
-    foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
-        $targetFile = $targetDirectory . basename($_FILES['files']['name'][$key]);
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['files'])) {
+        $fileStatus = array(); // une array qui contient les messages d'erreur
+        
+        foreach ($_FILES['files']['tmp_name'] as $key => $tmp_name) {
+            $targetFile = $targetDirectory . basename($_FILES['files']['name'][$key]);
 
         // cheque si le fichier existe déjà
         if (file_exists($targetFile)) {
@@ -40,9 +28,20 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['files'])) {
             if ($_FILES['files']['size'][$key] > $file_size) {
                 $fileStatus[] = "Le fichier " . $_FILES['files']['name'][$key] . " est trop lourd. 10Mo max";
             } else {
+                /*
+                si le cookie dossier_choisi existe et n'est pas vide, le fichier sera téléchargé dans le fichier choisi
+                sinon le fichier sera téléchargé dans le fichier uploads
+                */
+                if (isset($_COOKIE['dossier_choisi']) && !empty($_COOKIE['dossier_choisi'])) {
+                    $targetDirectory = 'uploads/' . $_COOKIE['dossier_choisi'] . '/';
+                }else{
+                    $targetDirectory = 'uploads/';
+                }
+                // la fonction move_uploaded_file() déplace un fichier téléchargé dans le $targetDirectory
                 if (move_uploaded_file($_FILES['files']['tmp_name'][$key], $targetDirectory . $_FILES['files']['name'][$key])) {
-                    $fileStatus[] = "Le fichier " . basename($targetDirectory) . " a été téléchargé avec succès.";
-                    // $fileStatus[] = "succès";
+                    // $fileStatus[] = "Le fichier " . $_FILES['files']['name'][$key] . " a été téléchargé avec succès.";
+                    $fileStatus[] = "succès";
+                    setcookie('dossier_choisi', '', time() - 3600, '/');
                 } else {
                     $fileStatus[] = "Erreur lors du téléchargement du fichier " . $_FILES['files']['name'][$key];
                 }
@@ -54,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['files'])) {
 
 // File Download
 if (isset($_GET['file'])) {
-    $file = 'uploads/' . basename($_GET['file']);
+    $file = $targetDirectory. basename($_GET['file']);
     if (file_exists($file)) {
         // Set appropriate headers for the file download
         header('Content-Type: application/octet-stream');
@@ -75,7 +74,7 @@ if (isset($_GET['file'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>maintenance</title>
     <link rel="stylesheet" href="../systeme/css/parisarebours.css">
-    <link rel="stylesheet" href="../systeme/css/folder-style.css">
+    <link rel="stylesheet" href="../systeme/css/tooltip.css">
 </head>
 <body>
     <!-- popup cree dossier -->
@@ -128,22 +127,10 @@ if (isset($_GET['file'])) {
         }
         ?>
     </ul>
-     <div class="folder-list">
-        <button class="folder-title"></button>
-        <ul class="bold parent-of-kids">
-        <li>
-            hello
-        </li>
-        </ul>
-        <button class="folder-title"></button>
-        <ul class="bold parent-of-kids">
-        <li>
-            hello
-        </li>
-        </ul>
-     </div>
+    
+    <p>Text <span class="tooltip">Explanation<span class="tooltiptext">Text explaining the text</span></span></p>
+
     <button id="popup_button">cree une dossier</button>
-    <script src="../systeme/js/folder-dir.js"></script>
     <script src="../systeme/js/drag-drop.js"></script>
     <script src="../systeme/js/maintenance.js"></script>
 </body>
